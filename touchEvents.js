c2560b487a994be14c528716b6698a506745cac1
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2024-05-16 20:03:22 -0400

    idk

--- touchEvents.js
+++ touchEvents.js
@@ -1,10 +1,16 @@
+let targetID;
+
 document.addEventListener("touchstart", (e) => {
   console.log("touchstart");
   document.body.style.backgroundColor = "red";
+  targetID = e.target.id;
 });
 document.addEventListener("touchmove", (e) => {
   console.log("touchmove");
   document.body.style.backgroundColor = "green";
+  const div = document.getElementById(targetID);
+  div.style.top = e.targetTouches[e.targetTouches.length - 1].clientY;
+  div.style.left = e.targetTouches[e.targetTouches.length - 1].clientX;
 });
 document.addEventListener("touchend", (e) => {
   console.log("touchend");
